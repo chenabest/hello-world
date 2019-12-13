4d86e4f40dc55e8c6675b285f80d83647d322a15
陈爱珍 <57119799+chenabest@users.noreply.github.com>
2019-12-14 00:44:02 +0800

    Update function_tools.py
    当遇到多层嵌套复杂的数据类型的变量焦头烂额时，试试用pretty_print的方法打印下，你会豁然开朗。

--- utils/function_tools.py
+++ utils/function_tools.py
@@ -1,72 +1,85 @@
-#!/usr/bin/python3
-# achen 2019/12/10 10:19 PM
+# achen 2019/11/28 1:24 PM
 # function_tools
-# /Users/achen/PythonProjects/chena_project/utils/function_tools.py
 
+import pendulum
+from collections import Iterable
 from traceback import print_exc
-import re
 
 
-BASIC = ('str', 'int', 'float', 'bool', 'list', 'dict', 'tuple', 'NoneType', 'type', 'set')
-
-
-def is_basic(name, basic=BASIC):
-    if eval('{}.__class__.__name__'.format(name)) in basic:
-        return True
+def get_timestamp_of_next_work_start_time(hour_start: int, minute_start=0, second_start=0):
+    now = pendulum.now()
+    hour_now = int(now.format('HH'))
+    minute_now = int(now.format('mm'))
+    second_now = int(now.format('ss'))
+    if hour_now >= hour_start and minute_now >= minute_start and second_now >= second_start:
+        timestamp = pendulum.now().add(days=1).replace(
+            hour=hour_start, minute=minute_start, second=second_start).timestamp()
     else:
+        timestamp = pendulum.now().replace(
+            hour=hour_start, minute=minute_start, second=second_start).timestamp()
+    return timestamp
+
+
+def is_valid_container(content):
+    if not content or isinstance(content, str) or not isinstance(content, Iterable):
         return False
+    else:
+        return True
 
 
-def show_dir_details(name, max_depth=1, re_filter='.*', re_exclude=None, special_cases=('__doc__$', ), ignore_basic=False):
+def pretty_print(content, prefix='', is_root_container=True):
     """
-    功能：显示一个函数、方法、类、模块、变量的所有相关的详细信息，帮助快速了解其用法。
-    :param name: 变量或类、方法等的名称
-    :param max_depth: 最大深度
-    :param re_filter: 过滤出想要看的信息
-    :param re_exclude: 在filter之后再排除一些模式
-    :param special_cases: 属于special_case的模式不会被exclude
-    :param ignore_basic: True,False或序列(列表、元组等)。表示忽略的类型，不显示其详细信息。False不忽略任何类型，True将忽略默认类型，序列将忽略指定类型。
-    :return: 
+    当遇到多层嵌套复杂的数据类型的变量焦头烂额时，试试用pretty_print的方法打印下，你会豁然开朗。
+    功能：美化容器对象（列表、字典等及其任意嵌套形式）的显示，不同的元素换行显示，并自动对齐同一层元素，实际使用时一般不用传prefix和is_root_container参数
+    :param content: 请传一个复杂的多层嵌套的组合数据对象
+    :param prefix: 递归时使用, 用于对齐
+    :param is_root_container: 递归时使用，当设置为False时，如果content是容器类型，会打印出content的类型
+    :return: None
     """
-    if max_depth == 0:
-        return
-    if ignore_basic:
-        if isinstance(ignore_basic, bool):
-            if is_basic(name):
-                return
-        elif is_basic(name, ignore_basic):
-            return
-    flag = False
-    for p in eval('dir(%s)' % name):
-        try:
-            if p not in ['__abstractmethods__'] and re.search(re_filter, f'{name}.{p}'):
-                if re_exclude:
-                    is_in_special_cases = False
-                    if special_cases:
-                        for case in special_cases:
-                            if re.search(case, f'{name}.{p}'):
-                                print(name + '.' + p + ':', eval(f'{name}.{p}'))
-                                flag = True
-                                is_in_special_cases = True
-                                break
-                    if not is_in_special_cases and not re.search(re_exclude, f'{name}.{p}'):
-                        print(name + '.' + p + ':', eval(f'{name}.{p}'))
-                        flag = True
-                else:
-                    print(name + '.' + p + ':', eval(f'{name}.{p}'))
-                    flag = True
-        except Exception as e:
-            print(f'获取信息：{name}.{p}出错')
-            print(e)
-            print_exc()
-            raise
-        show_dir_details(name + '.' + p, max_depth - 1, re_filter, re_exclude, special_cases, ignore_basic)
-    if flag:
-        print()
+    try:
+        if not is_valid_container(content):
+            print(prefix, end='')
+            print(content)
+        else:
+            if not is_root_container:
+                print(prefix, end='')
+                type_name = '<'+content.__class__.__name__+'>'
+                print(type_name)
+                prefix += ' '*(len(type_name)-1)  # 减1使与上一行最后一个字符对齐
+            if 'get' in dir(content):     # 字典型容器
+                for key in content:
+                    print(prefix + key + ':', end='')
+                    if content.get(key) == content:   # 避免递归死循环
+                        print('{...}')
+                    elif not is_valid_container(content.get(key)):
+                        print(content.get(key))
+                    else:
+                        type_name = '<'+content.get(key).__class__.__name__+'>'
+                        print(type_name)
+                        new_prefix = prefix + ' '*(len(key+':'))+' '*(len(type_name)-1)
+                        pretty_print(content.get(key), new_prefix, True)
+            else:  # 非字典型容器
+                for element in content:
+                    pretty_print(element, prefix, False)
+    except Exception as e:
+        import time
+        print(e)
+        time.sleep(3)
+        print_exc()
+        import ipdb
+        ipdb.set_trace()
+        raise
 
 
 if __name__ == '__main__':
-    a = 889
-    print(is_basic('a'))
-    show_dir_details('a', ignore_basic=True)
-    show_dir_details('str', max_depth=2, re_exclude='__', special_cases=('__doc__$', ))
+    pretty_print(988)
+    pretty_print(([1,2,3]))
+    lg = globals()
+    lg.update({'1111test': [1, 2, 3]})
+    ll = locals()
+    pretty_print([1, 2, 3, {'09wo', 78.99, 'fa'}, 3, 4, 5])
+    pretty_print(lg)
+    a = [1, 2, {'a22': 67, 'fff': 9.09}, 89293488989, [1, 2, (6, 'ff', 9)], 45, 'helloworld']
+    pretty_print(a)
+    import ipdb
+    ipdb.set_trace()
